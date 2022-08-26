323ff52adb058701337da02c80eab570cd64c038
hadh93 <hoondog1993@gmail.com>
2022-08-26 13:58:28 -0500

    JPARepository Error resolved. (Corrected project dependencies & Reloaded all Gradle Projects)

--- src/main/java/com/example/study_admin/repository/UserRepository.java
+++ src/main/java/com/example/study_admin/repository/UserRepository.java
@@ -4,8 +4,6 @@ import com.example.study_admin.model.entity.User;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
-
 }
