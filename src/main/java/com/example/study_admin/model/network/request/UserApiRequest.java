250908ffa264190fccb5304d5473b345ec4ed92a
hadh93 <hoondog1993@gmail.com>
2022-09-14 19:36:03 -0500

    8-1-04. 사용자 API UPDATE

--- src/main/java/com/example/study_admin/model/network/request/UserApiRequest.java
+++ src/main/java/com/example/study_admin/model/network/request/UserApiRequest.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -16,6 +18,10 @@ public class UserApiRequest {
     private String status;
     private String email;
     private String phoneNumber;
+
+    public LocalDateTime registeredAt;
+    public LocalDateTime unregisteredAt;
+
 }
 // Request-Response를 따로 관리하는 이유: 값이 동일하지 않을 수 있으며, 목적이 다르므로 별개의 클래스로 구분한다.
 // e.g. password는 request부에서는 평문, response부에서는 마스킹되어 받게 됨
