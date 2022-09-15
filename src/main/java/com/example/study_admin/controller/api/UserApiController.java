5cfcafd3471eef85220bef12d9d506ca534f7074
hadh93 <hoondog1993@gmail.com>
2022-09-14 19:49:31 -0500

    8-1-05. 사용자 API DELETE

--- src/main/java/com/example/study_admin/controller/api/UserApiController.java
+++ src/main/java/com/example/study_admin/controller/api/UserApiController.java
@@ -41,7 +41,8 @@ public class UserApiController implements CrudInterface <UserApiRequest, UserApi
 
     @Override
     @DeleteMapping("{id}") // /api/user/{id}
-    public Header delete(Long id) {
-        return null;
+    public Header delete(@PathVariable Long id) {
+        log.info("delete : {}", id);
+        return userApiLogicService.delete(id);
     }
 }
